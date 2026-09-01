17aafc6ae0f1451e9e6d496431eccf88710be262
Renato Soto <renatasoto.1inf@gmail.com>
2026-09-01 19:04:28 -0400

    Rama base

--- src/api/endpoints/points-of-interest.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from '../client';
-import { PointOfInterest } from '../../types';
-
-export const poiApi = {
-  list: (type?: string) => {
-    const url = type ? `/points-of-interest?type=${type}` : '/points-of-interest';
-    return apiClient.getAxiosInstance().get<PointOfInterest[]>(url);
-  },
-};
